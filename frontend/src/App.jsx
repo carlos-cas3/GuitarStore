569b24d7c4b2c5a52b5486ed2ef6c71f84bff9e8
carlos <ccastillob44@gmail.com>
2025-05-03 13:27:15 -0500

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { useRoutes } from 'react-router-dom'
+import routes from './routes/routes.js' 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const routing = useRoutes(routes)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
---
+++ frontend/src/routes/routes.js
@@ -0,0 +1,10 @@
+import RegisterFrom from '../features/auth/RegisterForm.jsx';
+
+const routes = [
+    {
+        path : '/register',
+        element : <RegisterFrom />,
+    },
+];
+
+export default routes;
